afc9497386c3ad3540b8bd8bf96e87ea37eebb2f
ililihayy <lilipushkar15@gmail.com>
2024-08-19 23:05:19 +0300

    added date selection

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
-todos = [{"task": "First Task", "done": False}, {"task": "Second Task", "done": False}]
+todos = [{"task": "First Task", "date": "2024-08-30", "done": False}]
 
 @app.route("/")
 def index():
     return render_template("index.html", todos=todos)
 
-
 @app.route("/add", methods=["POST"])
 def add():
     todo = request.form['todo_add']
-    todos.append({"task": todo, "done": False})
+    date = request.form['task_date']
+    todos.append({"task": todo, "date": date, "done": False})
     return redirect(url_for("index"))
 
 @app.route("/delete/<int:index>", methods=['POST'])
